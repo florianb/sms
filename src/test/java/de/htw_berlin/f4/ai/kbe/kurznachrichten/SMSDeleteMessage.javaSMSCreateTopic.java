bb43d4ffc6327b5758b25a1cbc7f2322e7f9bf9e
Florian Breisch <mail@florianbreisch.de>
2013-12-09 10:34:05 +0100

    created file-skeletons for test-cases

---
+++ src/test/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/SMSDeleteMessage.javaSMSCreateTopic.java
@@ -0,0 +1,15 @@
+package de.htw_berlin.f4.ai.kbe.kurznachrichten;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+@RunWith(JUnit4.class)
+public class SMSDeleteMessage
+{
+  @Test
+  thisIsAlwaysTrue()
+  {
+    assert(true);
+  }
+}
---
+++ src/test/java/de/htw_berlin/f4/ai/kbe/kurznachrichten/SMSDeleteUser.java
@@ -0,0 +1,15 @@
+package de.htw_berlin.f4.ai.kbe.kurznachrichten;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+@RunWith(JUnit4.class)
+public class SMSDeleteUser
+{
+  @Test
+  thisIsAlwaysTrue()
+  {
+    assert(true);
+  }
+}
